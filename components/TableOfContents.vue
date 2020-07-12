60ee626c2ab3d5c3f20a8f0759a4c3989699882e
Spencer Young <spjy@hawaii.edu>
2020-07-11 20:25:34 -1000

    feat: table of contents style

--- components/TableOfContents.vue
+++ components/TableOfContents.vue
@@ -2,15 +2,15 @@
   <div
     class="toc sticky-info description-card rounded shadow p-4 overflow-y-auto"
   >
-    <h1>{{ note.header }}</h1>
-    <p>{{ note.description }}</p>
+    <h1 class="header">{{ note.header }}</h1>
+    <p class="description">{{ note.description }}</p>
     <h2>
       Table of Contents
     </h2>
     <div
       v-for="content in toc"
       :key="content.anchor"
-      :class="content.level === 1 ? 'truncate font-bold' : 'truncate'"
+      :class="content.level === 1 ? 'truncate font-bold text-lg' : 'truncate'"
       :href="`#${content.anchor}`"
       :title="content.content"
       :style="`text-indent: ${0.8 * (content.level - 1)}em`"
@@ -40,5 +40,23 @@ export default {
 <style scoped>
 .toc {
   height: calc(100vh - 2em);
+}
+
+h1 {
+  border-image-source: none;
+  border-bottom: none;
+}
+
+.header {
+  border-top: 1px #a3f7bf solid;
+}
+
+.description {
+  border-bottom: 1px #a3f7bf solid;
+}
+
+a {
+  font-family: 'Libre Franklin';
+  color: white !important;
 }
 </style>
