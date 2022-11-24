b042ef967a7b7004dae52c05f50103f2e7dfb26c
Spencer Young <spjy@hawaii.edu>
2022-11-23 22:20:57 -0700

    Change text size in who section

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="flex justify-start overflow-auto">
-    <div v-for="(skill, key) in skills" :key="key" class="table text-lg">
+    <div v-for="(skill, key) in skills" :key="key" class="table">
       <div class="table-row-group">
         <div>
           <div
-            class="table-cell category px-4 py-4 uppercase text-lower font-mono"
+            class="table-cell category px-4 py-4 uppercase text-lower font-mono text-sm"
           >
             {{ key }}
           </div>
         </div>
         <div v-for="s in skill" :key="s">
-          <div class="table-cell px-4 text-sm">
+          <div class="table-cell px-4">
             {{ s }}
           </div>
         </div>
@@ -24,9 +24,9 @@ export default {
   props: {
     skills: {
       type: Object,
-      default: () => {},
-    },
-  },
+      default: () => {}
+    }
+  }
 }
 </script>
 
