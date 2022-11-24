1f2e041bcaf38803917fa2c6cf8eae67d912de95
Spencer Young <spjy@hawaii.edu>
2022-11-23 22:32:00 -0700

    Add spacing under intro

--- components/Intro.vue
+++ components/Intro.vue
@@ -2,7 +2,7 @@
   <div class="background w-full">
     <div class="flex justify-center">
       <div class="heading py-10 px-10">
-        <h1 class="title accent-green font-bold mb-2">
+        <h1 class="title accent-green font-bold mb-8">
           Hi, I am Spencer Young.
         </h1>
 
