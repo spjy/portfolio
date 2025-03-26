95b457326077a33f12b9531ef735169fffd06c9a
spjy <spjy@hawaii.edu>
2025-03-25 21:55:47 -0700

    Prevent markdown from affecting main page

--- components/homepage/Skills.vue
+++ components/homepage/Skills.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="flex justify-start overflow-auto pb-2">
-    <table class="table text-lg">
-      <tr class="table-header-group">
-          <th
+    <div class="text-lg">
+      <div class="table-header-group">
+          <div
             v-for="(_, key) in skills" :key="key" 
             class="text-left table-cell category px-4 py-4 uppercase text-lower font-mono text-sm"
           >
             {{ key }}
-          </th>
-        </tr>
-        <tr v-for="i in getMaxLengthSkills()" :key="i" class="table-row" style="white-space: nowrap;">
-          <td v-for="skill in skills" :key="skill" class="table-cell px-4 text-sm overflow-auto">
+          </div>
+        </div>
+        <div v-for="i in getMaxLengthSkills()" :key="i" class="table-row" style="white-space: nowrap;">
+          <div v-for="skill in skills" :key="skill" class="table-cell px-4 text-sm overflow-auto">
             {{ Object.values(skill)[i - 1] }}
-          </td>
-        </tr>
-      </table>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
